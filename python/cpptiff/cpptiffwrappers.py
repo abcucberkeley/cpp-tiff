94a16ec24d0f3951dd80ebb201d4d6544346a14f
MattHazard <mrm.mue@gmail.com>
2024-11-11 13:24:40 -0800

    Fixed the spacing in cpptiffwrappers.py

--- python/cpptiff/cpptiffwrappers.py
+++ python/cpptiff/cpptiffwrappers.py
@@ -2,15 +2,20 @@ import numpy as np
 import os
 from .cpptiff import pybind11_read_tiff, pybind11_write_tiff, pybind11_get_image_shape
 
+
 def read_tiff(file_name):
-	if not os.path.isfile(file_name):
-		raise Exception(f'{file_name} does not exist')
-	im = pybind11_read_tiff(file_name)
-	im = np.transpose(im, (2, 1, 0))
-	return im
+    if not os.path.isfile(file_name):
+        raise Exception(f'{file_name} does not exist')
+    im = pybind11_read_tiff(file_name)
+    im = np.transpose(im, (2, 1, 0))
+    return im
+
+
 def write_tiff(file_name, data, compression='lzw'):
-	data = np.transpose(data, (2, 1, 0))
-	pybind11_write_tiff(file_name, data, compression)
-	return
+    data = np.transpose(data, (2, 1, 0))
+    pybind11_write_tiff(file_name, data, compression)
+    return
+
+
 def get_image_shape(file_name):
-	return pybind11_get_image_shape(file_name)
+    return pybind11_get_image_shape(file_name)
