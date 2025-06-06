7eb69b98da3e6dd9b840131a4227f3b19a664dfa
xs254 <107810111+xs254@users.noreply.github.com>
2025-06-06 10:55:29 -0700

    Update tests.m
    Correct confusing naming of variable and title in the tutorial

--- mexSrc/tests.m
+++ mexSrc/tests.m
@@ -4,18 +4,18 @@ im(:,:,2:2:100) = 1;
 parallelWriteTiff('test.tif',im);
 %% parallelReadTiff in different modes
 imT = parallelReadTiff('test.tif');
-imT_1_5 = parallelReadTiff('test.tif',[1 6]); %[start end]
-imT_1_2_9 = parallelReadTiff('test.tif',[1 2 11]);%[start step end]
-imT_2_2_10 = parallelReadTiff('test.tif',[2 2 12]);%[start step end]
-%%
+imT_1_6 = parallelReadTiff('test.tif',[1 6]); %[start end]
+imT_1_2_11 = parallelReadTiff('test.tif',[1 2 11]);%[start step end]
+imT_2_2_12 = parallelReadTiff('test.tif',[2 2 12]);%[start step end]
+%% Visualize tiff read by different modes
 figure;
-montage(imT_1_5,'Size',[1 6]);
-title('1 to 5');
+montage(imT_1_6,'Size',[1 6]);
+title('1 to 6');
 figure;
-montage(imT_1_2_9,'Size',[1 6]);
-title('1 to 9 at step 2');
+montage(imT_1_2_11,'Size',[1 6]);
+title('1 to 11 at step 2');
 figure;
-montage(imT_2_2_10,'Size',[1 6]);
-title('2 to 10 at step 2');
+montage(imT_2_2_12,'Size',[1 6]);
+title('2 to 12 at step 2');
 %%
 imSize = getImageSizeMex('test.tif');
